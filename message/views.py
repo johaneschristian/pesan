9b1c8a60c371cd945644fd2de3218aba9313f344
JOHANES CHRISTIAN LEWI PUTRAEL TARIGAN <johanes.christian@ui.ac.id>
2022-01-13 23:22:20 +0700

    debugging deployment

--- message/views.py
+++ message/views.py
@@ -29,7 +29,7 @@ def login_page(request):
     #         #     print(user.id)
     #         #     return redirect('message:chat_window')
 
-    return HttpResponse('hello world~~')
+    return render(request, 'message/landing_page.html')
 
 # @login_required
 # def logout_mechanism(request):
