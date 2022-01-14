c8b66f56427355bf142186b3aa8f22d8851d6b98
JOHANES CHRISTIAN LEWI PUTRAEL TARIGAN <johanes.christian@ui.ac.id>
2022-01-14 10:48:53 +0700

    Uncomment

--- message/urls.py
+++ message/urls.py
@@ -6,11 +6,11 @@ app_name = 'message'
 
 urlpatterns = [
     path('', views.login_page, name='login_page'),
-    # path('logout/', views.logout_mechanism, name='logout'),
-    # path('register/', views.register_user, name='register_user'),
-    # path('chat/', views.chat_window, name='chat_window'),
-    # path('get-latest-chat/', views.get_latest_chats, name='get_latest_chat'),
-    # path('get-user-friends/', views.get_user_friends, name='get_user_friends'),
-    # path('get-messages/', views.get_messages, name='get_messages'),
-    # path('add-message/', views.add_messages, name='add_message'),
+    path('logout/', views.logout_mechanism, name='logout'),
+    path('register/', views.register_user, name='register_user'),
+    path('chat/', views.chat_window, name='chat_window'),
+    path('get-latest-chat/', views.get_latest_chats, name='get_latest_chat'),
+    path('get-user-friends/', views.get_user_friends, name='get_user_friends'),
+    path('get-messages/', views.get_messages, name='get_messages'),
+    path('add-message/', views.add_messages, name='add_message'),
 ]
